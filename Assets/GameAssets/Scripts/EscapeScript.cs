be4ec8948e665d1a42cf3e9b9349de8d8acf2eda
Florian Vick <florian@eagle-eye-studios.net>
2015-10-28 12:37:48 +0100

    Milestone 1

---
+++ Assets/GameAssets/Scripts/EscapeScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EscapeScript : MonoBehaviour {
+
+    public KeyCode Key;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(Key))
+        {
+            Application.LoadLevel(1);
+        }
+	}
+}
